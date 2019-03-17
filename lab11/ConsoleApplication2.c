1c7f421dc4f76f2fc66cb8f032d84981d80c09ea
vzezjulin <sodogewow@bk.ru>
2019-03-17 19:38:21 +0300

    release

---
+++ lab11/ConsoleApplication2.c
@@ -0,0 +1,91 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+int main()
+{
+	double maxd, maxdp = 0, *B, *X, *d, **A, ESP;
+	int i, j, n;
+	printf("Enter size of array: ");
+	scanf("%d", &n);
+
+	B = (double*)malloc(n * sizeof(double));
+	X = (double*)malloc(n * sizeof(double));
+	d = (double*)malloc(n * sizeof(double));
+	A = (double**)malloc(n * sizeof(double*));
+	for (i = 0; i < n; ++i)
+	{
+		B[i] = 0; X[i] = 0; d[i] = 0;
+		A[i] = (double*)malloc(n * sizeof(double));
+		for (j = 0; j < n; ++j)
+		{
+			A[i][j] = 0;
+		}
+	}
+
+	for (i = 0; i < n; ++i)
+	{
+		for (j = 0; j < n; ++j)
+		{
+			printf("A[%d][%d]=", i, j);
+			scanf("%lf", &A[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (i = 0; i < n; ++i)
+	{
+		printf("B[%d]=", i);
+		scanf("%lf", &B[i]);
+	}
+
+	printf("\nEnter measurement accurecy: ");
+	scanf_s("%lf", &ESP);
+
+	do
+	{
+		for (i = 0; i < n; ++i)
+		{
+			d[i] = X[i];
+			X[i] = B[i];
+			for (j = 0; j < n; ++j)
+			{
+				if (i != j)
+				{
+					X[i] -= A[i][j] * X[j];
+				}
+			}
+			X[i] = X[i] / A[i][i];
+			d[i] = fabs(d[i] - X[i]);
+		}
+		maxd = d[0];
+		for (i = 1; i < n; ++i)
+		{
+			if (d[i] > maxd) maxd = d[i];
+		}
+		if (maxdp == 0)
+		{
+			maxdp = maxd;
+			continue;
+		}
+		else
+		{
+			if (maxdp - maxd < 0)
+			{
+				break;
+			}
+			else
+			{
+				maxdp = maxd;
+			}
+		}
+	} while (maxd > ESP);
+
+	for (i = 0; i < n; ++i)
+	{
+		printf("X[%d]=%lf ", i, X[i]);
+	}
+	system("pause");
+}
---
+++ lab13/ConsoleApplication4.c
@@ -0,0 +1,87 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//#define SIZE(x)  (sizeof(x) / sizeof((x)[0]))
+
+void bubblesort_increase(int *parr, int N) { //по возврастаню
+	for (int i = 0; i < N - 1; ++i) {
+		for (int j = 0; j < N - 1; ++j) {
+			if (parr[j] > parr[j + 1]) {
+				int buf = parr[j];
+				parr[j] = parr[j + 1];
+				parr[j + 1] = buf;
+			}
+		}
+	}
+}
+
+void bubblesort_descending(int *parr, int N) { //по убыванию
+	for (int i = 0; i < N - 1; ++i) {
+		for (int j = 0; j < N - 1; ++j) {
+			if (parr[j] < parr[j + 1]) {
+				int buf = parr[j];
+				parr[j] = parr[j + 1];
+				parr[j + 1] = buf;
+			}
+		}
+	}
+}
+
+int main(int argc, const char * argv[]) {
+	int N = 0, M = 0;
+	printf("size of arrA: ");
+	scanf_s("%d", &N);
+	printf("size of arrB: ");
+	scanf_s("%d", &M);
+	int *arrA = (int*)malloc(N * sizeof(int));
+	int *arrB = (int*)malloc(M * sizeof(int));
+	srand(time(0));
+
+	for (int i = 0; i < N; ++i)
+		arrA[i] = rand() % 10;
+
+	for (int i = 0; i < M; ++i)
+		arrB[i] = rand() % 10;
+
+	bubblesort_increase(arrA, N);
+	bubblesort_descending(arrB, M);
+
+	printf("arrA: ");
+	for (int i = 0; i < N; ++i)
+		printf("%d ", arrA[i]);
+	printf("\n");
+
+	printf("arrB: ");
+	for (int i = 0; i < M; ++i)
+		printf("%d ", arrB[i]);
+	printf("\n");
+
+	int K = M + N;
+
+	int *arrC = (int*)malloc(K * sizeof(int));
+
+	int n = 0;
+	for (int i = 0; i < K / 2; i++) //слияние
+	{
+		arrC[n++] = arrA[i];
+		arrC[n++] = arrB[i];
+	}
+
+	/*int *arrC = (int*)malloc(SIZE(arrA) + SIZE(arrB));
+
+	int i = 0, j = 0, k = 0;
+
+	while (i < SIZE(arrA) && j < SIZE(arrB)) {
+		arrC[k++] = arrA[i] < arrB[j] ? arrA[i++] : arrB[j++];
+	}
+
+	while (i < SIZE(arrA)) arrC[k++] = arrB[i++];
+	while (j < SIZE(arrB)) arrC[k++] = arrB[j++];*/
+
+	bubblesort_increase(arrC, K);
+
+	printf("ArrC: ");
+	for (int i = 0; i < K; i++) printf("%d ", arrC[i]);
+	printf("\n");
+	return 0;
+}
